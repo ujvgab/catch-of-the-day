fd89cefb99833f6a4e31a7c3bd6db38cc5b02e2a
ujvgab <ujvgabor@gmail.com>
2020-02-10 15:27:22 +0100

    new components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
-ReactDOM.render(<StorePicker />, document.getElementById('main'));
+ReactDOM.render(<App />, document.getElementById('main'));
