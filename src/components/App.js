6ce1e8c7b7db7a4e65fce5c37a7fd75bf848e88c
ujvgab <ujvgabor@gmail.com>
2020-02-17 17:00:07 +0100

    editing fishes

--- src/components/App.js
+++ src/components/App.js
@@ -47,6 +47,15 @@ class App extends React.Component {
         });
     }
 
+    updateFish = (key, updatedFish) => {
+        //1. take a copy of the current state
+        const fishes = {...this.state.fishes};
+        //2. Update that state
+        fishes[key] = updatedFish;
+        //3. Set that to state
+        this.setState({ fishes });
+    }
+
     loadSampleFishes = () => {
         this.setState({
             fishes: sampleFishes
@@ -74,7 +83,7 @@ class App extends React.Component {
                     </ul>
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order} />
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />       
+                <Inventory addFish={this.addFish} updateFish={this.updateFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes}/>
             </div>
         )
     }
